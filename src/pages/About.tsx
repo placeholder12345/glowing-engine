5a30ccc295a1e7ee1b5ff7562ddb222d3bfd7ac4
khgaw <61212705+khgaw@users.noreply.github.com>
2020-12-18 21:08:05 -0500

    Add routing to home and about (#7)
    * Add routing to home and about
    
    * Change import order

---
+++ src/pages/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { EuiTitle } from '@elastic/eui';
+
+function About(): React.ReactElement {
+  return (
+    <EuiTitle size="l">
+      <h1>About</h1>
+    </EuiTitle>
+  );
+}
+
+export default About;
