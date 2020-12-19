96ed4e19cb93b633cdd84f9f536abecc8321118d
khgaw <kgaw2017@gmail.com>
2020-12-18 20:23:43 -0500

    Added Elastic UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { EuiButton, EuiFlexItem, EuiPage } from '@elastic/eui';
 
 function App(): React.ReactElement {
   return (
-    <div className="App">
-      <div className="content">Hello World</div>
-    </div>
+    <EuiPage>
+      <div className="App">
+        <div className="content">Hello World</div>
+        <EuiFlexItem grow={false}>
+          <EuiButton onClick={(): void => {}}>Primary</EuiButton>
+        </EuiFlexItem>
+      </div>
+    </EuiPage>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '@elastic/eui/dist/eui_theme_dark.css';
 
 ReactDOM.render(
   <React.StrictMode>
